8098e089509878f85a43b475643b1fe2f9e33652
adrianmcenturion <adrianmcenturion@gmail.com>
2022-11-02 13:03:41 -0300

    Added footer and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-const App = () => <div>App</div>
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
+
+const App = () => (
+  <>
+    <Header />
+    <div>Holaaa</div>
+    <Footer />
+  </>
+)
 
 export default App
